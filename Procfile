0010bbc7c043b9b3f88da2c07c813040883eb171
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-04 17:06:25 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m GilbertAnimeBot
+web: python3 -m GilbertAnimeBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Gilbert Robot
+:: Enables virtual env mode and then starts Gilbert
+env\scripts\activate.bat && py -m GilbertAnimeBot
